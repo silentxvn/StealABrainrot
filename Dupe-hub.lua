348e52c0accbcdbbaaf679b495c557864f6d2561
Silentx <silentxvn@gmail.com>
2025-11-06 12:20:35 +0700

    Update Dupe-hub.lua

--- Dupe-hub.lua
+++ Dupe-hub.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet('https://pastefy.app/AILhrbIZ/raw'))()
-local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/silentxvn/StealABrainrotDupe/main/Dupe-hub.lua"))()
+local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/silentxvn/StealABrainrotDupe/main/Dupe-hub.lua"))
